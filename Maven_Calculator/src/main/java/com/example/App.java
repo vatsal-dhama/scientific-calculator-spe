d3223397906f4c964a5da433542c6ad88312b49c
vatsal dhama <thevatsaldhama@gmail.com>
2023-10-31 01:07:37 +0530

    "Maven Project created"

---
+++ Maven_Calculator/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package Maven_Calculator.src.main.java.com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/main/java/Calculator.java
+++
@@ -1,6 +0,0 @@
-package src.main.java;
-public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
